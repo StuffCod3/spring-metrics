c45b39f437c41de6e715a9c803b6be697469b609
StuffCod3 <evgeny.mell@yandex.ru>
2024-01-28 18:33:19 +0300

    Initial commit

---
+++ src/main/java/ru/stuff/springmetrics/SpringMetricsApplication.java
@@ -0,0 +1,13 @@
+package ru.stuff.springmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMetricsApplication.class, args);
+    }
+
+}
